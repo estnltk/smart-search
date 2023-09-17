2b68a515725b2735589f71510dacbdaa93810f76
Tarmo Vaino <tarmo.vaino@gmail.com>
2023-09-17 17:00:29 +0300

    kohendatud ea_paring/*

--- api/ea_paring/docker-entrypoint.sh
+++ api/ea_paring/docker-entrypoint.sh
@@ -3,9 +3,9 @@ export WORKERS=${WORKERS:-1}
 export TIMEOUT=${TIMEOUT:-30}
 export WORKER_CLASS=${WORKER_CLASS:-sync}
 
-exec /usr/bin/tini -- venv/bin/gunicorn --bind=0.0.0.0:6608 \
+exec /usr/bin/tini -- venv/bin/gunicorn --bind=0.0.0.0:6602 \
     "--workers=${WORKERS}" \
     "--timeout=${TIMEOUT}" \
     "--worker-class=${WORKER_CLASS}" \
     --worker-tmp-dir=/dev/shm "$@" \
-    flask_api_paring_lemmad:app
+    flask_api_ea_paring:app
