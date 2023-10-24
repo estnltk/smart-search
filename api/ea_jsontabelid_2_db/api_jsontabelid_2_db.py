0b9f6615586565ad234d53145c13c9b4d3364bd5
Tarmo Vaino <tarmo.vaino@gmail.com>
2023-10-24 22:29:09 +0300

    ok versioon tabelitest andmebaasi tegemiseks

--- api/ea_jsontabelid_2_db/api_jsontabelid_2_db.py
+++ api/ea_jsontabelid_2_db/api_jsontabelid_2_db.py
@@ -12,9 +12,12 @@ Code:
         "program": "./api_jsontabelid_2_db.py",
         "args": [\
             "--verbose", \
-            "--db_name=test.sqlite", \
-            "--tables=ignoreeritavad_vormid:lemma_korpuse_vormid:lemma_kõik_vormid:liitsõnad:indeks_vormid:indeks_lemmad", \
-            "../ea_jsoncontent_2_jsontabelid/1017-government_orders.json" \
+            "--db_name=test-tmp.sqlite", \
+            "--tables=lemma_kõik_vormid:lemma_korpuse_vormid:indeks_vormid:indeks_lemmad:liitsõnad:kirjavead:allikad", \
+            "../ea_jsoncontent_2_jsontabelid/1024-government_orders.json",
+            "../ea_jsoncontent_2_jsontabelid/1024-government_regulations.json",  \
+            "../ea_jsoncontent_2_jsontabelid/1024-local_government_acts.json",
+            "../ea_jsoncontent_2_jsontabelid/1024-state_laws.json",
             ],
         "env": {}
     }
@@ -29,16 +32,14 @@ $ ./venv/bin/python3 ./api_jsontabelid_2_db.py \
     ../ea_jsoncontent_2_jsontabelid/${PREFIKS}*.json 
 
 SisendJson:
-    {   "indeks_vormid":[(VORM, DOCID, START, END, LIITSÕNA_OSA)],
-        "indeks_lemmad":[(LEMMA, DOCID, START, END, LIITSÕNA_OSA)],
-        "liitsõnad":[(OSALEMMA, LIITLEMMA)],
-        "lemma_kõik_vormid":[(VORM, LEMMA)],
-        "lemma_korpuse_vormid":[(VORM, 0, LEMMA)],
-        "kirjavead":[[VIGANE_VORM, VORM, KAAL]],
-        "kirjavead_2": [[typo, lemma, correct_wordform, confidence]]
-        "allikad":[(DOCID, CONTENT)],
-        "ignoreeritavad_vormid":[VORM]
-    }
+        {   "indeks_vormid":[(VORM, DOCID, START, END, LIITSÕNA_OSA)],
+            "indeks_lemmad":[(LEMMA, DOCID, START, END, LIITSÕNA_OSA)],
+            "liitsõnad":[(OSALEMMA, LIITLEMMA)],
+            "lemma_kõik_vormid":[(VORM, KAAL, LEMMA)],
+            "lemma_korpuse_vormid":[(LEMMA, KAAL, VORM)],
+            "kirjavead":[(VIGANE_VORM, VORM)],
+            "allikad":[(DOCID, CONTENT)],
+        }
  '''
 import os
 import sys
@@ -54,63 +55,19 @@ class DB:
         self.db_name = db_name
         self.tables = tables
 
-        if self.verbose:
-            sys.stdout.write("# teeme/avame andmebaasi ja tabelid")
-
-        # päringu normaliseerimine: päringusõned -> lemmad
-        # loome/avame andmebaasi
         if os.path.isfile(self.db_name):
+            if self.verbose:
+                sys.stdout.write(f"# kustutame andmebaasi {self.db_name}\n")
             os.remove(self.db_name)
+
+        if self.verbose:
+            sys.stdout.write(f"# teeme/avame andmebaasi {self.db_name}\n")
+
+        # loome/avame andmebaasi
         self.con_baas = sqlite3.connect(self.db_name)
         self.cur_baas = self.con_baas.cursor()
 
-        # {"tabelid":{ "kirjavead":[[VIGANE_VORM, VORM, KAAL]] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS kirjavead(
-            vigane_vorm TEXT NOT NULL,  -- sõnavormi vigane versioon
-            vorm TEXT NOT NULL,         -- korpuses esinenud sõnavorm
-            kaal INT,                   -- sagedasemad vms võiksid olla suurema kaaluga
-            PRIMARY KEY(vigane_vorm, vorm)
-        )''')
-        
-        # {"tabelid":{ "kirjavead_2": [[typo, lemma, correct_wordform, confidence]] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS kirjavead_2(
-            typo TEXT NOT NULL,              -- vigane versioon korrektsest sõnavormist
-            lemma TEXT NOT NULL,             -- lemma
-            correct_wordform TEXT NOT NULL,  -- korrektne sõnavorm            
-            confidence INT,                  -- korrektse sõnavormi esinemiste arv
-            PRIMARY KEY(typo, lemma, correct_wordform)
-        )''')       
-        
-        # {"tabelid":{ "ignoreeritavad_vormid":[VORM] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS ignoreeritavad_vormid(
-            ignoreeritav_vorm TEXT NOT NULL,  -- sellist sõnavormi ignoreerime päringus
-            paritolu INT NOT NULL,            -- ignoreerimise põhjus int'iks kodeerituna                       
-            PRIMARY KEY(ignoreeritav_vorm)
-        )''')       
-
-        # {"tabelid":{ "lemma_korpuse_vormid":[(LEMMA, VORM)] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS lemma_korpuse_vormid(
-            lemma TEXT NOT NULL,        -- dokumendis esinenud sõnavormi lemma
-            vorm TEXT NOT NULL,         -- lemma need sõnavormid, mis on mingis dokumendis dokumendis esinenud
-            PRIMARY KEY(lemma, vorm)
-        )''')
-
-        # {"tabelid":{ "lemma_kõik_vormid":[(VORM, 0, LEMMA)] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS lemma_kõik_vormid( 
-            vorm TEXT NOT NULL,         -- lemma kõikvõimalikud vormid genereerijast
-            paritolu INT NOT NULL,      -- 0-lemma on leitud jooksvas dokumendis olevst sõnavormist; 1-vorm on lemma sünonüüm
-            lemma TEXT NOT NULL,        -- korpuses esinenud sõnavormi lemma
-            PRIMARY KEY(vorm, lemma)
-        )''')
-
-        # {"tabelid":{ "liitsõnad":[(OSALEMMA, LIITLEMMA)] }
-        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS liitsõnad( 
-            osalemma TEXT NOT NULL,     -- liitsõna osasõna lemma
-            liitlemma TEXT NOT NULL,    -- liitsõna osasõna lemmat sisaldav liitsõna lemma
-            PRIMARY KEY(osalemma, liitlemma)
-        )''')
-
-        # {"tabelid":{ "indeks_vormid":[(VORM, DOCID, START, END, LIITSÕNA_OSA)] }
+        # "indeks_vormid":[(VORM, DOCID, START, END, LIITSÕNA_OSA)]
         self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS indeks_vormid(
                 vorm  TEXT NOT NULL,          -- (jooksvas) dokumendis esinenud sõnavorm
                 docid TEXT NOT NULL,          -- dokumendi id
@@ -121,7 +78,7 @@ class DB:
                 )
         ''')
 
-        # {"tabelid":{ "indeks_lemmad":[(LEMMA, DOCID, START, END, LIITSÕNA_OSA)] }
+        # "indeks_lemmad":[(LEMMA, DOCID, START, END, LIITSÕNA_OSA)]
         self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS indeks_lemmad(
                 lemma  TEXT NOT NULL,         -- (jooksvas) dokumendis esinenud sõna lemma
                 docid TEXT NOT NULL,          -- dokumendi id
@@ -131,6 +88,37 @@ class DB:
                 PRIMARY KEY(lemma, docid, start, end)
                 )
         ''')
+
+        # "liitsõnad":[(OSALEMMA, LIITLEMMA)]
+        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS liitsõnad( 
+            osalemma TEXT NOT NULL,     -- liitsõna osasõna lemma
+            liitlemma TEXT NOT NULL,    -- liitsõna osasõna lemmat sisaldav liitsõna lemma
+            PRIMARY KEY(osalemma, liitlemma)
+        )''')
+
+        # "lemma_kõik_vormid":[(VORM, KAAL, LEMMA)],
+        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS lemma_kõik_vormid( 
+            vorm TEXT NOT NULL,         -- lemma kõikvõimalikud vormid genereerijast
+            kaal INT NOT NULL,          -- suurem number on sagedasem
+            lemma TEXT NOT NULL,        -- korpuses esinenud sõnavormi lemma
+            PRIMARY KEY(vorm, lemma)
+        )''')
+        
+        # "lemma_korpuse_vormid":[(LEMMA, KAAL, VORM)]
+        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS lemma_korpuse_vormid(
+            lemma TEXT NOT NULL,        -- dokumendis esinenud sõnavormi lemma
+            kaal INT NOT NULL,          -- suurem number on sagedasem
+            vorm TEXT NOT NULL,         -- lemma need sõnavormid, mis on mingis dokumendis dokumendis esinenud
+            PRIMARY KEY(lemma, vorm)
+        )''')
+
+        # {"tabelid":{ "kirjavead":[[VIGANE_VORM, VORM, KAAL]] }
+        self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS kirjavead(
+            vigane_vorm TEXT NOT NULL,  -- sõnavormi vigane versioon
+            vorm TEXT NOT NULL,         -- korpuses esinenud sõnavorm
+            PRIMARY KEY(vigane_vorm, vorm)
+        )''')
+
 
         # ["tabelid"]["allikad"]:[(DOCID, CONTENT)]
         self.cur_baas.execute('''CREATE TABLE IF NOT EXISTS allikad(
@@ -161,52 +149,55 @@ class DB:
         """Kanna self.json_in'ist info andmbeaaaside tabelitesse
         """
         for table in self.tables:
-            pass
-            if table == "kirjavead":
-                # {"tabelid":{ "kirjavead":[[VIGANE_VORM, VORM, KAAL]] }
+            pass #DB
+            if table == "lemma_kõik_vormid":
+                # "lemma_kõik_vormid":[(VORM, KAAL, LEMMA)],
                 pbar = tqdm(self.json_in["tabelid"][table], desc=f'# {file} : {table} :', disable=(not self.verbose))
-                for vigane_vorm, vorm, kaal in pbar:
-                    res = self.cur_baas.execute(f"SELECT * FROM {table} WHERE vigane_vorm='{vigane_vorm}' and vorm='{vorm}'")
+                for vorm, kaal, lemma in pbar:
+                    res = self.cur_baas.execute(f"SELECT vorm, kaal, lemma FROM {table} WHERE vorm='{vorm}' and lemma='{lemma}'")
                     if len(res_fetchall:=res.fetchall()) > 0:
                         #selline juba oli, liidame kokku
-                        if kaal > 0: # kaal muutub
-                            for ret_vigane_vorm, ret_vorm, ret_kaal in res_fetchall:
+                        assert len(res_fetchall) == 1, \
+                            f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
+                        uus_kaal = 0
+                        if kaal > 0: # muutub raskemaks
+                            for ret_vorm, ret_kaal, ret_lemma in res_fetchall:
+                                assert vorm==ret_vorm and lemma==ret_lemma, \
+                                    f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
                                 uus_kaal = kaal + ret_kaal
-                                self.cur_baas.execute(f"UPDATE {table} SET kaal='{uus_kaal}' WHERE vigane_vorm='{vigane_vorm}' and vorm='{vorm}'")
+                                self.cur_baas.execute(f"UPDATE {table} SET kaal='{uus_kaal}' WHERE vorm='{vorm}' and lemma='{lemma}'")
                     else:
                         # polnud, lisame uue
-                        self.cur_baas.execute(f"INSERT INTO {table} VALUES('{vigane_vorm}','{vorm}',{kaal})")
-            elif table == "kirjavead_2":
-                # {"tabelid":{ "kirjavead_2": [[typo, lemma, correct_wordform, confidence]] }
+                        rec = (vorm, kaal, lemma)
+                        self.cur_baas.execute(f"INSERT INTO {table} VALUES {rec}")
+            elif table == "lemma_korpuse_vormid":
+                # "lemma_korpuse_vormid":[(LEMMA, KAAL, VORM)]
                 pbar = tqdm(self.json_in["tabelid"][table], desc=f'# {file} : {table} :', disable=(not self.verbose))
-                for typo, lemma, correct_wordform, confidence in pbar:     
-                    if typo == "EEesti": #DB
-                        pass  #DB
-                    res = self.cur_baas.execute(f"SELECT typo, lemma, correct_wordform, confidence FROM {table} WHERE typo='{typo}' and lemma='{lemma}' and correct_wordform ='{correct_wordform}'")
-                    res_fetchall = res.fetchall()
-                    if len(res_fetchall) > 0:
+                for lemma, kaal, vorm in pbar:
+                    res = self.cur_baas.execute(f"SELECT lemma, kaal, vorm FROM {table} WHERE lemma='{lemma}' and vorm='{vorm}'")
+                    if len(res_fetchall:=res.fetchall()) > 0:
                         #selline juba oli, liidame kokku
-                        assert len(res_fetchall) == 1, f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
-                        if confidence > 0: # kaal muutub
-                            ret_typo, ret_lemma, ret_correct_wordform, ret_confidence = res_fetchall[0]
-                            new_confidence = confidence + ret_confidence
-                            self.cur_baas.execute(f"UPDATE {table} SET confidence='{new_confidence}' WHERE typo='{typo}' and lemma='{lemma}' and correct_wordform ='{correct_wordform}'")
-                    else: # polnud, lisame uue
-                        try:
-                            self.cur_baas.execute(f"INSERT INTO {table} VALUES('{typo}','{lemma}','{correct_wordform}', '{confidence}')")
-                            self.con_baas.commit()
-                        except Exception as e:
-                            assert False, f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
+                        assert len(res_fetchall) == 1, \
+                            f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
+                        uus_kaal = 0
+                        if kaal > 0: # muutub raskemaks
+                            for ret_lemma, ret_kaal, ret_vorm in res_fetchall:
+                                assert vorm==ret_vorm and lemma==ret_lemma, \
+                                    f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
+                                uus_kaal = kaal + ret_kaal
+                                self.cur_baas.execute(f"UPDATE {table} SET kaal='{uus_kaal}' WHERE lemma='{lemma}' and vorm='{vorm}'")
+                    else:
+                        # polnud, lisame uue
+                        rec = (lemma, kaal, vorm)
+                        self.cur_baas.execute(f"INSERT INTO {table} VALUES {rec}")
             else:
                 pbar = tqdm(self.json_in["tabelid"][table], desc=f'# {file} : {table} :', disable=(not self.verbose))
                 for rec in pbar:
                     try:
-                        #self.cur_baas.execute(f'INSERT INTO {table} VALUES({values_pattern})', rec)
-                        test = f"INSERT INTO {table} VALUES{tuple(rec)}"
-                        self.cur_baas.execute(f"INSERT INTO {table} VALUES{tuple(rec)}")
+                        self.cur_baas.execute(f"INSERT INTO {table} VALUES {tuple(rec)}")
                     except Exception as e:
                         # assert False, f'assert {getframeinfo(currentframe()).filename}:{getframeinfo(currentframe()).lineno}'  #DB
-                        continue # selline juba oli
+                        continue # selline juba oli, ignoreerime kordusi
             self.con_baas.commit()         
         
     def string2json(self, str:str)->Dict:
